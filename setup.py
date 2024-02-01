51978139c8504f7ec5f5f55d116f9bfabaf9a7b2
kerky <78611077+Kerkpower@users.noreply.github.com>
2024-02-01 20:44:29 +0200

    Update setup.py
    im very bad at this

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='python-ip-check',
     version='0.1.0',
-    packages=['main'],
+    packages=['ip_check'],
     url='https://github.com/Kerkpower/python-ip-check',
     license='Apache License 2.0',
     author='kerky',
