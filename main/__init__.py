10466f45f2531bdb50d23bf2962559e7d816bbb7
kerky <78611077+Kerkpower@users.noreply.github.com>
2024-02-01 20:45:57 +0200

    Delete main directory
    aaaaaaaaaaaaaaa

--- main/__init__.py
+++
@@ -1,9 +0,0 @@
-def is_valid_ip(ip):
-    """Check if the provided IP address is valid."""
-    octets = ip.split(".")
-
-    if len(octets) != 4 or not all(octet.isdigit() and 0 <= int(octet) <= 255 for octet in octets) or any(
-            int(octet) > 255 for octet in octets):
-        return False
-
-    return True
