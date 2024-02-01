d1768b7e293af097f213c743265ceea00064967e
kerkpower <kerkpowerfort@gmail.com>
2024-02-01 20:35:57 +0200

    add file to github

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python-ip-check',
+    version='0.1.0',
+    packages=['main'],
+    url='https://github.com/Kerkpower/python-ip-check',
+    license='Apache License 2.0',
+    author='kerky',
+    author_email='',
+    description='a bad python module to check if an ip is valid'
+)
